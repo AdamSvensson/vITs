edb4a0956940306f2b621fda67d8397c15dbf3ef
Klas Tärnström <klas@tarnstrom.se>
2015-04-01 11:05:00 +0200

    Lagt till en MainFrame class.

---
+++ src/MainFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by klas_000 on 2015-04-01.
+ */
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        setDefaultCloseOperation(3);
+        setTitle("vITs Resehantering");
+
+    }
+
+}
